abe7eb65bdd35aa8ce8f692524aaaf3dfa69d92f
msionut28 <moloceaionutt@gmail.com>
2024-01-04 18:36:58 +0200

    safari fully functional, can switch between tabs

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import HomePage from "./pages/HomePage/HomePage";
 import Projects from "./pages/Projects/Projects";
-import Test from "./pages/Test/Test"
+import Test from "./pages/Test/Test";
 import Browser from "./pages/Browser/Browser";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        {/* <NavBar/> */}
+        <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/projects" element={<Projects />} />
--- src/pages/Browser/Browser.jsx
+++ src/pages/Browser/Browser.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
+import {
+  ChevronLeft,
+  ChevronRight,
+  Lock,
+  RotateCw,
+  Share,
+  Plus,
+  Layers,
+} from "react-feather";
 import "./style.css";
-import { ChevronLeft } from "react-feather";
-import { ChevronRight } from "react-feather";
 
 const Browser = () => {
+  const [selectedTab, setSelectedTab] = useState(1);
+  const selectedTabSettings = {
+    0: {
+      text: "BattleShip Game",
+      url: "https://msionut28.github.io/battleships_browserGame/",
+    },
+    1: { text: "PantryChef", url: "https://pantrychef-ga.netlify.app" },
+    2: { text: "SafeHaven", url: "https://safehaven-ga.netlify.app" },
+    3: {
+      text: "SquadSprint",
+      url: "https://squadsprintfe-production.up.railway.app",
+    },
+  };
+
+  const handleTabClick = (index) => {
+    setSelectedTab(index);
+  };
   return (
     <div className="safari">
-      <div className="topSection">
+      <div className="top-section">
         <div className="buttons">
           <button className="close"></button>
           <button className="minimize"></button>
@@ -18,12 +43,49 @@ const Browser = () => {
           </div>
         </div>
         <div className="search">
-            <input type="text" />
+          <div className="search-bar">
+            <span className="lock-sign">
+              <Lock size={12} style={{ marginRight: "0.25vw" }} />
+            </span>
+            <span className="search-input">
+              {selectedTabSettings[selectedTab]?.url || ""}
+            </span>
+          </div>
+          <span className="refresh">
+            <RotateCw size={16} />
+          </span>
+        </div>
+        <div className="right-hand-symbols">
+          <span className="share">
+            <Share size={18} />
+          </span>
+          <span className="plus">
+            <Plus size={18} />
+          </span>
+          <span className="layers">
+            <Layers size={18} />
+          </span>
         </div>
       </div>
-      <iframe src="https://pantrychef-ga.netlify.app/" title="my first project"
-      width={1200}
-      height={1000}></iframe>
+      <div className="tabs">
+        {Object.keys(selectedTabSettings).map((index) => (
+          <div
+            key={index}
+            className={`tab ${selectedTab === index ? "selected" : ""}`}
+            onClick={() => handleTabClick(index)}
+          >
+            {selectedTabSettings[index].text}
+          </div>
+        ))}
+      </div>
+      <div className="website">
+        <iframe
+          src={selectedTabSettings[selectedTab]?.url || ""}
+          title="my first project"
+          width="100%"
+          height="100%"
+        />
+      </div>
     </div>
   );
 };
